d0eab60e41a8233af2021a19f4d2022c40fe37a0
hzyfaa <ahuzayfa14@gmail.com>
2025-04-22 00:11:22 -0600

    refactor/clean-main
    added comments, removed unused velocityScale constant

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,34 @@
 
 #include "simulation.h"
 
-const float velocityScale = 1.0f;
-
 int main() {
+  // window for rendering the simulation
   sf::RenderWindow window(sf::VideoMode(900, 900), "N-Body Simulation");
+  // set frame rate to 60 FPS
   window.setFramerateLimit(60);
-
+  // initialize the simulation object
   Simulation simulation;
-
+  // variables used for tracking mouse drag
   bool isDragging = false;
   sf::Vector2f dragStart, dragEnd;
 
   // *** main loop ***
   while (window.isOpen()) {
     sf::Event event;
+    // handle events
     while (window.pollEvent(event)) {
+      // exit app
       if (event.type == sf::Event::Closed) window.close();
-
-      // on click : spawn new body
+      // left mouse click: spawn a new body and begin tracking drag
       if (event.type == sf::Event::MouseButtonPressed &&
           event.mouseButton.button == sf::Mouse::Left) {
         dragStart = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
         isDragging = true;
+        // * new body at the drag start position (initially static)
         simulation.addBody(dragStart, {0.f, 0.f});
       }
 
-      // on release : launch body with initial velocity
+      // mouse release : launch body with initial velocity
       if (event.type == sf::Event::MouseButtonReleased &&
           event.mouseButton.button == sf::Mouse::Left && isDragging) {
         dragEnd = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
@@ -36,16 +38,16 @@ int main() {
         isDragging = false;
       }
     }
-
+    // update physics
     simulation.update();
+    // remove bodies outside canvas
     simulation.clearOffScreenBodies(window);
-
+    // return bodies vector
     const auto& bodies = simulation.getBodies();
-
+    // clear screen + render state
     window.clear();
-
     for (const auto& body : bodies) body.draw(window);
-
+    // draw visual vector line while user is dragging
     if (isDragging) {
       dragEnd = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
       sf::Vertex line[] = {sf::Vertex(dragStart, sf::Color::Green),
